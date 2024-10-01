c3fb3f618a462e78c0004a86c179c5305765723e
LycanthroLabs <lycanthrolabs@proton.me>
2024-09-30 17:59:56 -0700

    Delete fixTags.js

--- fixTags.js
+++
@@ -1,7 +0,0 @@
-  document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('code').forEach(function(block) {
-      block.innerHTML = block.innerText
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-    });
-  });
